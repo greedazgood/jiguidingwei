034a494b333e3d3c16c9d12eadae2caddceba3c1
greedazgood <sunmail93@gmail.com>
2021-03-03 17:37:52 +0800

    fix:修改转发地址

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 return [
     //对外端口号8888 ip默认为本机ip
-    'url' => 'http://www.baidu.com',
+    'url' => 'http://112.64.125.182:8083/cdcs/trigger/flow',
     'interval' => 3000,//向后台发送数据时间间隔 单位ms
 ];
